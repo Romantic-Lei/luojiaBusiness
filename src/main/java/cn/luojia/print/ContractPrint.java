4ee1a268e787dfa55d24fc16f40981951881b911
Memories6 <1371910434@qq.com>
2020-02-13 18:17:10 +0800

    合同打印样式构造完成

--- src/main/java/cn/luojia/print/ContractPrint.java
+++ src/main/java/cn/luojia/print/ContractPrint.java
@@ -1,6 +1,7 @@
 package cn.luojia.print;
 
-import java.text.ParseException;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,6 +10,16 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFPatriarch;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.CellRangeAddress;
+
+import cn.luojia.util.DownloadUtil;
 import cn.luojia.util.UtilFuns;
 import cn.luojia.util.file.PoiUtil;
 import cn.luojia.vo.ContractProductVO;
@@ -22,7 +33,7 @@ import cn.luojia.vo.ContractVO;
  */
 public class ContractPrint {
 
-	public void print(ContractVO contract, String path, HttpServletResponse response) throws ParseException {
+	public void print(ContractVO contract, String path, HttpServletResponse response) throws Exception {
 		System.out.println(contract.getId());
 
 		SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -102,19 +113,221 @@ public class ContractPrint {
 				}
 			}
 			// 操作完第二款货物
-			pageMap.put("ContractDesc", stars+" 货物描述");			//重要程度 + 货物描述
+			pageMap.put("ContractDesc", stars + " 货物描述"); // 重要程度 + 货物描述
 			pageList.add(pageMap);
 		}
-		
+
 		// 定义货物图片的高度
 		int cellHeight = 96;
 		// 如果想区别对待货物为一款或者两款时的样式，我们可以再次下功夫
-//		if (contract.getPrintStyle().equals("2")) {
-//			cellHeight = 96; // 值可以自己另行设置
-//		}
-		
-		PoiUtil 
+		// if (contract.getPrintStyle().equals("2")) {
+		// cellHeight = 96; // 值可以自己另行设置
+		// }
 
+		PoiUtil poiUtil = new PoiUtil();
+
+		// 获取模板
+		HSSFWorkbook wb = new HSSFWorkbook(new FileInputStream(tempXlsFile));
+		HSSFFont defaultFont10 = poiUtil.defaultFont10(wb); // 设置字体
+		HSSFFont defaultFont12 = poiUtil.defaultFont12(wb); // 设置字体
+		HSSFFont blackFont = poiUtil.blackFont12(wb); // 设置字体
+		Short rmb2Format = poiUtil.rmb2Format(wb); // 设置格式
+		Short rmb4Format = poiUtil.rmb4Format(wb); // 设置格式
+
+		HSSFSheet sheet = wb.getSheetAt(0); // 选择第一个工作簿
+		wb.setSheetName(0, "购销合同"); // 设置工作簿的名称
+
+		CellRangeAddress region = null;
+		HSSFPatriarch patriarch = sheet.createDrawingPatriarch(); // add picture
+
+		HSSFRow nRow = null;
+		HSSFCell nCell = null;
+		int curRow = 0;
+
+		
+}
+
+	private HSSFCellStyle leftStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		curStyle.setWrapText(true); // 换行
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+		// fTitle.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); //加粗
+		curFont.setFontHeightInPoints((short) 10);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		curStyle.setBorderLeft(HSSFCellStyle.BORDER_THIN); // 实线右边框
+		curStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN); // 实线右边框
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle headStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("楷体");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curFont.setItalic(true);
+		curFont.setFontHeightInPoints((short) 16);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle tipStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("Georgia");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); // 加粗
+		curFont.setFontHeightInPoints((short) 28);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle addressStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("宋体");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		// fTitle.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); //加粗
+		curFont.setFontHeightInPoints((short) 10);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle ltdStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("Times New Roman");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); // 加粗
+		curFont.setItalic(true);
+		curFont.setFontHeightInPoints((short) 16);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle telStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("宋体");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		// fTitle.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); //加粗
+		curFont.setFontHeightInPoints((short) 9);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle titleStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("黑体");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); // 加粗
+		curFont.setFontHeightInPoints((short) 18);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle requestStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		curStyle.setWrapText(true); // 换行
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("宋体");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curFont.setFontHeightInPoints((short) 10);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle dutyStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("黑体");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); // 加粗
+		curFont.setFontHeightInPoints((short) 16);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	private HSSFCellStyle noteStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("宋体");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); // 加粗
+		curFont.setFontHeightInPoints((short) 12);
+		curStyle.setFont(curFont);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	public HSSFCellStyle thStyle(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("宋体");
+		curFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); // 加粗
+		curFont.setFontHeightInPoints((short) 12);
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curStyle.setBorderTop(HSSFCellStyle.BORDER_THIN); // 实线右边框
+		curStyle.setBorderRight(HSSFCellStyle.BORDER_THIN); // 实线右边框
+		curStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN); // 实线右边框
+		curStyle.setBorderLeft(HSSFCellStyle.BORDER_THIN); // 实线右边框
+
+		curStyle.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
+	}
+
+	public HSSFCellStyle bcv12(HSSFWorkbook wb) {
+		HSSFCellStyle curStyle = wb.createCellStyle();
+		HSSFFont curFont = wb.createFont(); // 设置字体
+		curFont.setFontName("Times New Roman");
+		curFont.setCharSet(HSSFFont.DEFAULT_CHARSET); // 设置中文字体，那必须还要再对单元格进行编码设置
+
+		curFont.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD); // 加粗
+		curFont.setFontHeightInPoints((short) 12);
+		curStyle.setFont(curFont);
+
+		curStyle.setBorderTop(HSSFCellStyle.BORDER_THIN); // 实线
+		curStyle.setBorderRight(HSSFCellStyle.BORDER_THIN); // 粗实线
+		curStyle.setBorderBottom(HSSFCellStyle.BORDER_THIN); // 实线
+		curStyle.setBorderLeft(HSSFCellStyle.BORDER_THIN); // 实线
+
+		curStyle.setAlignment(HSSFCellStyle.ALIGN_RIGHT);
+		curStyle.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER); // 单元格垂直居中
+
+		return curStyle;
 	}
 
 }
