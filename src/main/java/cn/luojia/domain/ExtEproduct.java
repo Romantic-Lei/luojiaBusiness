bfe58cc53d687734cf1d50fbb26cdc9887436087
Memories6 <1371910434@qq.com>
2020-02-15 16:28:54 +0800

    报运附件实体

---
+++ src/main/java/cn/luojia/domain/ExtEproduct.java
@@ -0,0 +1,30 @@
+package cn.luojia.domain;
+
+import lombok.Data;
+
+/**
+ * 
+ * @author luojiaBusiness
+ * @CreateDate 2020年2月15日
+ * @Description 报运附件实体类
+ */
+@Data
+public class ExtEproduct {
+	private String id;
+	
+	private String exportProductId;			//关联关系的表（外键），都成为普通字段
+	private String factoryId;
+	
+	private String factoryName;					//厂家名称  冗余
+	private String ctype;						//类型
+	
+	private String productNo;					//货号
+	private String productImage;				//货物照片
+	private String productDesc;					//货物描述
+	private Integer cnumber;					//数量
+	private String packingUnit;					//包装单位
+	private Double price;						//价格
+	private Double amount;						//总金额
+	private String productRequest;				//要求
+	private Integer orderNo;					//排序号
+}
