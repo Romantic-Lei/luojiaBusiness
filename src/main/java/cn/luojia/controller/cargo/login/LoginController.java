3d40c8684b4debd921922f23ac0fb5baa5768793
Memories6 <1371910434@qq.com>
2020-02-23 00:01:37 +0800

    退出登录

--- src/main/java/cn/luojia/controller/cargo/login/LoginController.java
+++ src/main/java/cn/luojia/controller/cargo/login/LoginController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import cn.luojia.domain.UserLogin;
 import cn.luojia.service.UserLoginService;
@@ -53,6 +55,19 @@ public class LoginController {
 			// 用户没有输入密码
 			return "/index.jsp";
 		}
+	}
+	
+	// 用户登出清除session
+	@RequestMapping("/logout.action")
+	public String logout(SessionStatus sessionStatus, HttpServletRequest request) {
+		
+		HttpSession session = request.getSession();
+		session.removeAttribute("name");
+		//	    注销用户，使session失效。
+		request.getSession().invalidate();
+		sessionStatus.setComplete();
+		
+		return "/index.jsp";
 	}
 	
 }
